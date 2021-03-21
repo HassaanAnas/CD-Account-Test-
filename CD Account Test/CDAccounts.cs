8cf9b2d0b7febc5a2b3477ffe6b3a3ca5ea9af05
Hassaan Anas <hassaananas@usf.edu>
2021-03-20 21:48:57 -0400

    Add project files.

---
+++ CD Account Test/CDAccounts.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CD_Account_Test
+{
+    class CDAccounts : SavingsAccount
+    {
+        // Field
+        private string _maturityDate;
+
+        // Constructor
+        public CDAccounts ()
+        {
+            _maturityDate = "";
+        }
+
+        // MaturityDate property
+        public string MaturityDate
+        {
+            get { return _maturityDate; }
+            set { _maturityDate = value; }
+        }
+    }
+}
---
+++ CD Account Test/SavingsAccount.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CD_Account_Test
+{
+    class SavingsAccount
+    {
+        // fields
+        private string _accountNumber;
+        private decimal _interestRate;
+        private decimal _balance;
+
+        //constructor
+        public SavingsAccount ()
+        {
+            _accountNumber = "";
+            _interestRate = 0;
+            _balance = 0;
+
+        }
+
+        // Account Number
+        public string AccountNumber
+        {
+            get { return _accountNumber; }
+            set { _accountNumber = value; }
+        }
+
+        // InterestRate Property
+        public decimal InterestRate
+        {
+            get { return _interestRate; }
+            set { _interestRate = value; }
+
+        }
+
+        // Balance Property
+        public decimal Balance
+        {
+            get { return _balance; }
+            set { _interestRate = value; }
+                
+        }
+    }
+}
